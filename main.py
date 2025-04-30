5ff26ec4f0ad65bc38d295462248bf1f2cd32408
nbarnabee <nicole.barnabee@gmail.com>
2025-04-30 21:02:08 +0200

    Build out full functionality

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import sys
 from stats import get_book_text
 from stats import get_word_count
+from stats import get_character_count
+from stats import make_sorted_list
+from stats import pretty_print_list
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
-    num_words = get_word_count(text)
-    print(f"{num_words} words found in the document")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        text = get_book_text(sys.argv[1])
+        num_words = get_word_count(text)
+        character_count = get_character_count(text)
+        character_list = make_sorted_list(character_count)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        pretty_print_list(character_list)
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,28 @@ def get_book_text(path) :
 
 def get_word_count(string) :
     return len(string.split())
+
+def get_character_count(string):
+    character_dict = {}
+    for c in string:
+        character = c.lower()
+        if character_dict.get(character):
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+    return character_dict
+
+def make_sorted_list(character_dict):
+    character_list = []
+    for key, value in character_dict.items():
+        if key.isalpha():
+            character_list.append({"char": key, "num": value})
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def pretty_print_list(list):
+    for item in list:
+        print(f"{item['char']}: {item['num']}")
